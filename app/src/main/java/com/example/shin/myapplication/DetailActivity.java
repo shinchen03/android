5e8195270fe2620defa12bb5871ab6f8920ee1ed
shin <shinchen03@gmail.com>
2018-05-10 23:48:51 -0500

    add some pic
    add some pic

--- app/src/main/java/com/example/shin/myapplication/DetailActivity.java
+++ app/src/main/java/com/example/shin/myapplication/DetailActivity.java
@@ -1,7 +1,12 @@
 package com.example.shin.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +14,43 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent in = getIntent();
+        int index = in.getIntExtra("com.example.shin.Item_Index", -1);
+
+        if (index > -1) {
+            int pic = getImg(index);
+            ImageView img = (ImageView) findViewById(R.id.imageView4);
+            scaleImg(img, pic);
+        }
+    }
+
+    private int getImg(int index) {
+        switch (index) {
+            case 0: return R.drawable.iphone_x;
+            case 1: return R.drawable.xperia_zx2;
+            case 2: return R.drawable.p10;
+            default: return -1;
+        }
+    }
+
+    private void scaleImg(ImageView img, int pic) {
+        // scale the img to fit the screen
+        Display screen = getWindowManager().getDefaultDisplay();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), pic, options);
+
+        int imgWidth = options.outWidth;
+        int screenWidth = screen.getWidth();
+
+        if (imgWidth > screenWidth) {
+            int ratio = Math.round( (float)imgWidth / (float) screenWidth );
+            options.inSampleSize = ratio;
+            options.inJustDecodeBounds = false;
+            Bitmap scaledImg = BitmapFactory.decodeResource(getResources(), pic, options);
+            img.setImageBitmap(scaledImg);
+        }
     }
 }
